f71a54bb3e49efc4c2d7dba227870c3535254b12
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-08 20:03:27 +0700

    membuat program node nya

---
+++ singlelinkedlist.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class node 
+{
+    public :
+        int noMhs;
+        node *next;
+
+};
+
+class linkedlist 
+{
+    node *START;
+
+public :
+    linkedlist()
+    {
+        START = NULL;
+    }
+
+    void addnote()
+    {
+        int nim;
+        cout << "\nMasukkan nomor mahasiswa: ";
+        cin >> nim;
+
+        node *nodebaru = new node;
+        nodebaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs )
+        {
+            if  ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak dijinkan\n";
+                return;
+            }
+            nodebaru->next = START;
+            START = nodebaru;
+            return;
+            
+        }
+
+        node *previous = START;
+        node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current-> noMhs )
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+
+        }
+
+        nodebaru->next = current;
+        previous->next = nodebaru;
+        
+    }
+}
