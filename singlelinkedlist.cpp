af0538143b424137deb139c3661aef0fe360ebbe
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-08 20:44:01 +0700

    membuat case 4

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -170,7 +170,30 @@ int main ()
             }
             break;
 
-            
+            case '4':
+            {
+                if (mhs.listEmpty() == true)
+                {
+                    cout << "\nList kosong\n";
+                    break;
+                }
+                node *previous, *current;
+                cout << endl
+                    << "Masukkan no mahasiswa yang  dicari : ";
+                cin >> nim;
+                if (mhs.search(nim, &previous, &current) == false)
+                    cout << endl
+                        << "Data tidak ditemukan" << endl;
+                else 
+                {
+                    cout << endl
+                        << "Data ditemukan" << endl;
+                    cout << "\nNo mahasiswa: "<< current->noMhs << endl;
+                    cout << "\n";
+
+                }
+            }
+            break;
         }
     }
 }
