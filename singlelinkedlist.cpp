560227b11fcb19a10a33ca4e03adf66aa1ac1ec8
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-08 20:11:19 +0700

    membuat listempty nya dan searchnya

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -59,6 +59,27 @@ public :
 
         nodebaru->next = current;
         previous->next = nodebaru;
-        
+
     }
+
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search (int nim, node **previous, node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)-> noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+
+    }
+    
 }
